b6347536edbdb323ad5cc50d6de0fbf3ea0b0d91
SandraClodion <clodionsandra@gmail.com>
2020-06-15 10:56:23 +0200

    Update entrepots_et_clients.py

--- entrepots_et_clients.py
+++ entrepots_et_clients.py
@@ -2,16 +2,17 @@ import numpy as np
 import pandas as pd
 import random
 
-def clients_aleatoires(df_points, k) :
+def clients_aleatoires(k, df_points = df_garage_entrepôts) :
 
     """
     Fonction qui ajoute à une dataframe de points géographiques un certain nombre de points
     géographiques représentant des clients
 
     La fonction prend en entrée :
+    - k : le nombre de clients qu'on veut générer aléatoirement
     - df_points : une dataframe de points géographiques (par exemple le garage et les entrepôts)
     Cette dataframe doit contenir des colonnes "Lat" et "Lon"
-    - k : le nombre de clients qu'on veut générer aléatoirement
+    
 
     La fonction retourne :
     - df_complete : la dataframe complétée par les clients aléatoires 
