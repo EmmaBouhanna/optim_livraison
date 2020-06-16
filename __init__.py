188e055b56e8af9a21b8ccb1a8559fa32af6541d
SandraClodion <clodionsandra@gmail.com>
2020-06-16 19:23:32 +0200

    update fichier routes, entrepots_et_clients et fichier csv warehouses

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 PATH = 'C:\Didou\Pro\devoirs\Mines\Projet info S2\optim_livraison'
+
 import numpy as np
 import pandas as pd
 import csv
@@ -7,5 +8,6 @@ import os
 import random
 import csv
 from deap import tools, creator, base
+
 # import pygraphviz as pgv
 # from PIL import Image
