8bbd7c3503b9e701f2a3ceced4ac25e36bc06a76
SandraClodion <clodionsandra@gmail.com>
2020-06-26 16:29:05 +0200

    ajout __init__

---
+++ __init__.py
@@ -0,0 +1,13 @@
+PATH = 'C:\Didou\Pro\devoirs\Mines\Projet info S2\optim_livraison'
+
+import numpy as np
+import pandas as pd
+import csv
+from uuid import uuid4
+import os
+import random
+import csv
+from deap import tools, creator, base
+
+# import pygraphviz as pgv
+# from PIL import Image
