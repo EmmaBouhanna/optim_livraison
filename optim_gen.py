37e67e73524737e028a696471de0e4318bc9b0f1
EmmaBouhanna <emma.bou_hanna@mines-paristech.fr>
2020-06-29 21:26:14 +0200

    autopep8

--- optim_gen.py
+++ optim_gen.py
@@ -1,8 +1,8 @@
 from __init__ import *
-from graphe_sans_osmnx import * 
-service_time = (1/6) # 10 min lost per delivery
-time_work = 8.0 # number of work hours
-cost_dist = 1/3600 # coût par unité de temps
+from graphe_sans_osmnx import *
+service_time = (1/6)  # 10 min lost per delivery
+time_work = 8.0  # number of work hours
+cost_dist = 1/3600  # unit time cost
 
 '''
 This part is coded in case we want to consider that all the trucks don't start their day directly
@@ -43,6 +43,8 @@ def truck_division(file_properties):
     return(number_truck_per_warehouse)
 
 '''
+
+
 def no_client_to_deliver(file):
     '''
     Determines if there are packages to deliver from each warehouse at the beginning of the day
@@ -53,12 +55,13 @@ def no_client_to_deliver(file):
     allowed in each warehouse and the number of deliveries per warehouse if non zero.
     '''
     file_2 = []
-    for i in range(1, len(file)//3 +1):
+    for i in range(1, len(file)//3 + 1):
         if file[3*i-1] != 0:
             file_2.append(file[3*(i-1)])
             file_2.append(file[3*i-2])
             file_2.append(file[3*i-1])
     return(file_2)
+
 
 def one_client_to_deliver(file):
     '''
@@ -70,21 +73,22 @@ def one_client_to_deliver(file):
     allowed in each warehouse and the number of deliveries per warehouse if non 1
     '''
     file_2 = []
-    for i in range(1, len(file)//3 +1):
+    for i in range(1, len(file)//3 + 1):
         if file[3*i-1] == 1:
-            instance = pd.read_csv(os.path.join(PATH,'input_data',file[3*(i-1)]))
+            instance = pd.read_csv(os.path.join(
+                PATH, 'input_data', file[3*(i-1)]))
             columns_res = ['Camion 1']
-            res = [instance['Identifiant'][i] for i in [0,1,0]]
+            res = [instance['Identifiant'][i] for i in [0, 1, 0]]
             print(res)
-            res = pd.DataFrame(res, columns = columns_res)
-            res.to_csv(os.path.join(PATH,'output_data', 'res_entrepot_' + str(instance['Identifiant'][0]) +'.csv' ))
-        else :
+            res = pd.DataFrame(res, columns=columns_res)
+            res.to_csv(os.path.join(PATH, 'output_data',
+                                    'res_entrepot_' + str(instance['Identifiant'][0]) + '.csv'))
+        else:
             file_2 += file[3*(i-1):3*i]
     return(file_2)
 
 
-
-def ind2route(individual, instance, distance_matrix, vehicle_capacity, max_vehicle, initCost = 0.0, unitCost = cost_dist, serviceTime = service_time):
+def ind2route(individual, instance, distance_matrix, vehicle_capacity, max_vehicle, initCost=0.0, unitCost=cost_dist, serviceTime=service_time):
     """
     Decoding individual to route
 
@@ -114,8 +118,11 @@ def ind2route(individual, instance, distance_matrix, vehicle_capacity, max_vehic
         demand = instance['demand'][customerID]
         updatedVehicleLoad = vehicleLoad + demand
         # Update elapsed time
-        returnTime = distance_matrix[customerID][0]*unitCost #distance_matrix = distance entre i et j
-        updatedElapsedTime = elapsedTime + distance_matrix[lastCustomerID][customerID]*unitCost + serviceTime + returnTime
+        # distance_matrix = distance entre i et j
+        returnTime = distance_matrix[customerID][0]*unitCost
+        updatedElapsedTime = elapsedTime + \
+            distance_matrix[lastCustomerID][customerID] * \
+            unitCost + serviceTime + returnTime
         # Validate vehicle load and elapsed time
         if (updatedVehicleLoad <= vehicleCapacity) and (updatedElapsedTime <= time_work):
             # Add to current sub-route
@@ -140,13 +147,14 @@ def ind2route(individual, instance, distance_matrix, vehicle_capacity, max_vehic
         raise ValueError
     return (route)
 
+
 def printRoute(route, merge=False):
     """
     Print a route with the journey of each truck (to check)
 
     Input : a route (list)
     Output : None 
-    """ 
+    """
     routeStr = '0'
     subRouteCount = 0
     for subRoute in route:
@@ -163,6 +171,7 @@ def printRoute(route, merge=False):
         print(routeStr)
     return
 
+
 def evalVRPTW(individual, instance, distance_matrix, vehicle_capacity, max_vehicle, unitCost=cost_dist, initCost=0.0):
     """
     Creation of a cost function based on the total cost of each route
@@ -178,8 +187,9 @@ def evalVRPTW(individual, instance, distance_matrix, vehicle_capacity, max_vehic
 
     Output : Fitness (1/Cost)
     """
-   
-    route = ind2route(individual, instance, distance_matrix, vehicle_capacity, max_vehicle)
+
+    route = ind2route(individual, instance, distance_matrix,
+                      vehicle_capacity, max_vehicle)
     totalCost = 0
     for subRoute in route:
         subRouteDistance = 0
@@ -197,14 +207,16 @@ def evalVRPTW(individual, instance, distance_matrix, vehicle_capacity, max_vehic
             # Update last customer ID
             lastCustomerID = customerID
         # Calculate transport cost
-        subRouteDistance = subRouteDistance + distance_matrix[lastCustomerID][0]
+        subRouteDistance = subRouteDistance + \
+            distance_matrix[lastCustomerID][0]
         subRouteTranCost = initCost + unitCost * subRouteDistance
         # Update total cost
         totalCost = totalCost + subRouteTranCost
     fitness = 1.0 / totalCost
-    return fitness 
+    return fitness
 
 # We intend to maximise fitness
+
 
 def cx_partialy_matched(ind1, ind2):
     '''
@@ -230,21 +242,24 @@ def cx_partialy_matched(ind1, ind2):
             ind2.append(gene)
     return ind1, ind2
 
+
 def mut_inverse_indexes(individual):
     '''
-    Step of genetic algorithm : Mutation (only by inversion, to keep the unicity of each occurence) . No insertion or deletion allowed
+    Step of genetic algorithm : Mutation (only by inversion, to keep the unicity of each occurence) . 
+    No insertion or deletion allowed
 
     Input : individual (list)
     Output : mutated individual
     '''
 
     start, stop = sorted(random.sample(range(len(individual)), 2))
-    individual = individual[:start + 1] + individual[stop:start:-1] + individual[stop+1:]
+    individual = individual[:start + 1] + \
+        individual[stop:start:-1] + individual[stop+1:]
     return (individual, )
 
 
-def run_vrptw(instance, distance_matrix, vehicle_capacity, max_vehicle, ind_size, pop_size, \
-    cx_pb, mut_pb, n_gen, init_cost = 0.0, unit_cost = cost_dist):
+def run_vrptw(instance, distance_matrix, vehicle_capacity, max_vehicle, ind_size, pop_size,
+              cx_pb, mut_pb, n_gen, init_cost=0.0, unit_cost=cost_dist):
     """
     Genetic algorithm which combines all the step of the process of evolution
 
@@ -263,24 +278,28 @@ def run_vrptw(instance, distance_matrix, vehicle_capacity, max_vehicle, ind_size
     - unit_cost (float) : temporal cost of 1 unity of distance
     """
 
-    creator.create("FitnessMax", base.Fitness, weights = (1.0,) ) #Positive weights because we intend to maximise fitness
+    # Positive weights because we intend to maximise fitness
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
     creator.create('Individual', list, fitness=creator.FitnessMax)
-    
+
     # Instance of Toolbox object
-    toolbox = base.Toolbox() 
+    toolbox = base.Toolbox()
 
     # Creation of generator that generates the numbers of customers to deliver (coded from 1 to ind_size, potentially to decode)
-    toolbox.register('indexes', random.sample, range(1, ind_size + 1), ind_size)
+    toolbox.register('indexes', random.sample,
+                     range(1, ind_size + 1), ind_size)
 
     # Creation of an indicidual by calling random.sample(range(1,ind_size+1), ind_size)
-    toolbox.register('individual', tools.initIterate, creator.Individual, toolbox.indexes)
+    toolbox.register('individual', tools.initIterate,
+                     creator.Individual, toolbox.indexes)
 
     # Creation of a population made up of n individuals : start of the genetic algorithm
-    toolbox.register('population', tools.initRepeat, list, toolbox.individual) 
-    pop = toolbox.population(n=pop_size) 
+    toolbox.register('population', tools.initRepeat, list, toolbox.individual)
+    pop = toolbox.population(n=pop_size)
 
     # Cost Evaluation
-    toolbox.register('evaluate', evalVRPTW, instance=instance, distance_matrix=distance_matrix, vehicle_capacity = vehicle_capacity, max_vehicle = max_vehicle,unitCost=unit_cost, initCost=init_cost)
+    toolbox.register('evaluate', evalVRPTW, instance=instance, distance_matrix=distance_matrix,
+                     vehicle_capacity=vehicle_capacity, max_vehicle=max_vehicle, unitCost=unit_cost, initCost=init_cost)
 
     # Selection of k individuals from the population
     toolbox.register('select', tools.selRoulette)
@@ -290,34 +309,36 @@ def run_vrptw(instance, distance_matrix, vehicle_capacity, max_vehicle, ind_size
 
     # Mutation of an individual
     toolbox.register('mutate', mut_inverse_indexes)
-    
+
     print('start of evolution')
     print(pop)
     fitnesses = list(map(toolbox.evaluate, pop))
-    for ind, fit in zip(pop, fitnesses): #Keep track of each indiviual's cost as attributes of creator Individual
+    # Keep track of each indiviual's cost as attributes of creator Individual
+    for ind, fit in zip(pop, fitnesses):
         ind.fitness.values = (fit,)
     print(f'Evaluated {len(pop)} individuals')
 
     # Begin the evolution
     for gen in range(n_gen):
         print(f'-- Generation {gen} --')
-    
+
         # Select the next generation individuals
-        offspring = toolbox.select(pop, len(pop)) # Every individual is able to move on to the next generation
+        # Every individual is able to move on to the next generation
+        offspring = toolbox.select(pop, len(pop))
 
         # Clone the selected individuals (to avoid modification of the real pop)
         offspring = list(map(toolbox.clone, offspring))
-        
 
         # Apply crossover and mutation on the offspring
-        for child1, child2 in zip(offspring[::2], offspring[1::2]): #list[start : stop : step]
+        # list[start : stop : step]
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
             # potentially match individual n and individual n+1
-            if random.random() < cx_pb: # Probability of a crossover
+            if random.random() < cx_pb:  # Probability of a crossover
                 toolbox.mate(child1, child2)
                 del child1.fitness.values
                 del child2.fitness.values
                 # changed child1 et child2, they now have invalid fitness.values (children have crossed)
-        for mutant in offspring: # Probability of mutation
+        for mutant in offspring:  # Probability of mutation
             if random.random() < mut_pb:
                 toolbox.mutate(mutant)
                 del mutant.fitness.values
@@ -331,14 +352,18 @@ def run_vrptw(instance, distance_matrix, vehicle_capacity, max_vehicle, ind_size
 
         # The population is entirely replaced by the offspring
         pop[:] = offspring
-        best_ind = tools.selBest(pop, 1)[0]  #returns a list containing the k best individuals among the population, here the best one
-        print(f'Best Fitness at Generation {gen}: {best_ind.fitness.values[0]}')
+        # returns a list containing the k best individuals among the population, here the best one
+        best_ind = tools.selBest(pop, 1)[0]
+        print(
+            f'Best Fitness at Generation {gen}: {best_ind.fitness.values[0]}')
 
     print('-- End of (successful) evolution --')
-    best_ind = tools.selBest(pop, 1)[0]  #returns a list containing the k best individuals among the population, here the best one
+    # returns a list containing the k best individuals among the population, here the best one
+    best_ind = tools.selBest(pop, 1)[0]
     print(f'Best individual: {best_ind}')
     print(f'Total cost: {1 / best_ind.fitness.values[0]}')
-    return( ind2route(best_ind, instance, distance_matrix, vehicle_capacity, max_vehicle))
+    return(ind2route(best_ind, instance, distance_matrix, vehicle_capacity, max_vehicle))
+
 
 def decode_to_GPS(liste_res, instances):
     """
@@ -351,16 +376,17 @@ def decode_to_GPS(liste_res, instances):
 
     """
     warehouse_num = 0
-    for (routes_warehouse,instance) in zip(liste_res,instances):
+    for (routes_warehouse, instance) in zip(liste_res, instances):
         warehouse_num += 1
         for route in routes_warehouse:
             for i in range(len(route)):
                 route[i] = int(instance['Identifiant'][route[i]])
-        name = 'res_entrepot_' + str(instance['Identifiant'][0]) +'.csv'
-        columns_res = ['camion' + str(k+1) for k in range(len(routes_warehouse))]
+        name = 'res_entrepot_' + str(instance['Identifiant'][0]) + '.csv'
+        columns_res = ['camion' + str(k+1)
+                       for k in range(len(routes_warehouse))]
         print(routes_warehouse)
-        res = pd.DataFrame(routes_warehouse, index = columns_res).transpose()
-        res.to_csv(os.path.join(PATH,'output_data',name))
+        res = pd.DataFrame(routes_warehouse, index=columns_res).transpose()
+        res.to_csv(os.path.join(PATH, 'output_data', name))
 
 
 def simulation_vrptw(garage, truck, number_clients):
@@ -380,38 +406,43 @@ def simulation_vrptw(garage, truck, number_clients):
     G.make_dist_matrix(df)
 
     # Save dataframe, dist_matrix and itineraries_dict
-    df.to_csv(os.path.join(PATH,'output_data_bis/df_complete.csv'))
+    df.to_csv(os.path.join(PATH, 'output_data_bis/df_complete.csv'))
 
-    if G.matrix is not None :
-        np.savetxt(os.path.join(PATH, 'output_data_bis/corrected_travel_times_array.csv'), G.matrix, delimiter=",")
+    if G.matrix is not None:
+        np.savetxt(os.path.join(
+            PATH, 'output_data_bis/corrected_travel_times_array.csv'), G.matrix, delimiter=",")
 
-    if G.itineraries is not None :
-        with open(os.path.join(PATH,'output_data_bis/itineraries_dict.csv'), 'w') as f:  
+    if G.itineraries is not None:
+        with open(os.path.join(PATH, 'output_data_bis/itineraries_dict.csv'), 'w') as f:
             w = csv.DictWriter(f, G.itineraries.keys())
             w.writeheader()
             w.writerow(G.itineraries)
 
     file_properties = generate_csv(G, df, indexes)
-    vehicle_capacity= file_properties.pop()
+    vehicle_capacity = file_properties.pop()
     file_properties = no_client_to_deliver(file_properties)
     file_properties = one_client_to_deliver(file_properties)
     trucks = file_properties[1::3]
     number_clients_per_warehouse = file_properties[2::3]
-    instances = [] #listes pour regrouper les résultats par entrepot
-    liste_res =[]
+    instances = []  # list to put together the results for each warehouse
+    liste_res = []
     for (i, file) in enumerate(file_properties[::3]):
         instance = pd.read_csv(os.path.join(PATH, 'input_data', file))
-        if instance.shape[0]>2 :
+        if instance.shape[0] > 2:
             instances += [instance]
         print(instance.head())
         max_vehicle = trucks[i]
-        instance_bis = instance.drop(['Unnamed: 0', 'Identifiant', 'latitude', 'longitude'], axis = 'columns')
+        instance_bis = instance.drop(
+            ['Unnamed: 0', 'Identifiant', 'latitude', 'longitude'], axis='columns')
         n = instance_bis.shape[1]
         number_of_points = n - 1
         number_of_clients = number_clients_per_warehouse[i]
-        instance_bis.columns = ['demand'] + [i for i in range(number_of_points)]
-        distance_matrix = instance_bis[[i for i in range(0,number_of_points)]] #prend la matrice des colonnes
-        res = run_vrptw(instance_bis, distance_matrix, vehicle_capacity, max_vehicle, number_of_clients, 100, 0.4, 0.2, 200)
+        instance_bis.columns = ['demand'] + \
+            [i for i in range(number_of_points)]
+        distance_matrix = instance_bis[[i for i in range(
+            0, number_of_points)]]  # columns matrix
+        res = run_vrptw(instance_bis, distance_matrix, vehicle_capacity,
+                        max_vehicle, number_of_clients, 100, 0.4, 0.2, 200)
         liste_res.append(res)
 
     decode_to_GPS(liste_res, instances)
